6a9c05e9765ded48fe3ae00fdb1d2621178a873c
Saif Ul Islam <saifulislam84210@gmail.com>
2020-04-30 14:48:54 +0500

    Basic attributes, systemInfo

--- include/dashboard/systemInfo.h
+++ include/dashboard/systemInfo.h
@@ -1,8 +1,24 @@
 #ifndef SYSTEM_INFO_H
 #define SYSTEM_INFO_H
 
+#include <QString>
+#include <string>
+#include <cpuid.h>
+
 struct systemInfoData {
     // some info here
+};
+
+struct kernelInfo {
+
+    std::string kernelName;         //! kernel name
+    std::string nodename;           //! nodename
+    std::string kernel_version;     //! kernel version
+    std::string machine;            //! machine
+    std::string processor;          //! processor
+    std::string hardware_platform;  //! hardware platform
+    std::string operating_system;   //! the operating system
+
 };
 
 class systemInfo
@@ -10,12 +26,41 @@ class systemInfo
 
     public:
 
-        int num;
+        systemInfo(void);
+
+        //! the system hostname
+        std::string hostName;    
+        
+        //!  the platform / architecture
+        std::string platform;
+
+        //! The linux distro
+        std::string distribution;
+
+        //! kernel info
+        kernelInfo kernel_info;
+        
+        //! the cpu model
+        std::string cpuModel;
+
+        //! the number of cpu cores
+        int cpuCores;
+
+        //! the CPU speed
+        std::string cpuSpeed;
+
         void testing_public(void);
 
     private:
     
         void testing(void);
 };
+
+void systemInfo::testing_public(void)
+{
+
+
+
+}
 
 #endif
--- src/dashboard/systemInfo.cpp
+++ src/dashboard/systemInfo.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include "../../include/dashboard/systemInfo.h"
 
+//!< the construtor for the class
+systemInfo::systemInfo(void)
+{
+
+}
+
 void systemInfo::testing_public(void)
 {
     printf("Hello, World!");
